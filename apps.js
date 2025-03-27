18cc2a2693e703f6bf602f4d3606c1a5e271cfba
Luiz Otavio <luiz.otav.2009@gmail.com>
2025-03-27 09:20:29 -0300

    centralized footer

--- apps.js
+++ apps.js
@@ -12,19 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.getElementById("calcularBtn").addEventListener("click", function() {
-    // Captura os valores dos inputs
     let n = parseFloat(document.getElementById("n").value);
     let e = parseFloat(document.getElementById("e").value);
     
-    // Verifica se n é um número válido
     if (isNaN(n) || isNaN(e)) {
         alert("Por favor, insira valores numéricos válidos.");
         return;
     }
     
-    // Calcula a carga elétrica (Q)
     let Q = n * e;
     
-    // Exibe o resultado na tela
-    document.getElementById("resultado").textContent = Q.toExponential(6); // Formato científico
+    document.getElementById("resultado").textContent = Q.toExponential(6); 
 });
